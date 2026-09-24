9c591a309fc90be346b1bc632d10685c0f92ade8
Kiyoshi Akila Tira <kiyoshi.atira@gmail.com>
2026-09-24 17:38:09 +0700

    feat(SRS01-03) :Reservation for user

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\AdminFacilityController;
 use App\Http\Controllers\PetugasReservationController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserReservationController;
+use App\Http\Controllers\FacilityController;
+use App\Http\Controllers\ReservationController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -66,3 +68,6 @@ Route::middleware('auth')->group(function () {
     });
 
 });
+Route::get('/fasilitas/{facility}', [FacilityController::class, 'show'])->name('facilities.show');
+
+Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
